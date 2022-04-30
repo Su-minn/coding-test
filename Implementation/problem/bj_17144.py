4a44cd2518871dd4b2c4aa4cef0e0b043bdefa2a
Su-minn <smsj68@gmail.com>
2022-05-01 03:35:55 +0900

    220501: Update Implementation Problem

--- Implementation/problem/bj_17144.py
+++ Implementation/problem/bj_17144.py
@@ -12,37 +12,72 @@ from jussuit_notes_ps.utils import *
 @time_check
 def solution(*args, **kwargs):
     import sys
+    import copy
 
     # 1) Input Values
     ## Input R, C, T
     R, C, T = map(int, input().split())
 
     ## Input A_(r,c)
-    board = [list(map(int, sys.stdin.readline().split())) for _ in range(R)]
-    print(board)
+    board = []
+    up_cleaner = -1
 
-    # 2) Spread fine dust
-    for nx, row in enumerate(board):
-        for ny, cell in enumerate(row):
-            ## If the cell has an air cleaner or is empty -> pass
-            if cell == -1 or cell == 0:
-                continue
-            else:
-                ## for the four directions
-                spread_air = board[nx][ny] // 5
-                for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                    temp_x, temp_y = nx + dx, ny + dy
-                    ## nx, ny is in the board, not air cleaner
-                    if (0 <= temp_x <= R-1 and 0 <= temp_y <= C-1) and board[temp_x][temp_y] != -1:
-                        board[nx][ny] -= spread_air
-                        board[temp_x][temp_y] += spread_air
+    for idx, _ in enumerate(range(R)):
+        row = list(map(int, sys.stdin.readline().split()))
+        if row[0] == -1:
+            if up_cleaner == -1: up_cleaner, down_cleaner = idx, idx + 1
 
-    # 3) Operate air cleaner
-    answer = True
+        board.append(row)
 
-    print(board)
+    # During time T
+    for _ in range(T):
+        # 2) Spread fine dust
+        temp_board = copy.deepcopy(board)
 
+        for nx, row in enumerate(board):
+            for ny, cell in enumerate(row):
+                ## If the cell has an air cleaner or is empty -> pass
+                if cell == -1 or cell == 0:
+                    continue
+                else:
+                    ## for the four directions
+                    spread_air = temp_board[nx][ny] // 5
+                    for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                        temp_x, temp_y = nx + dx, ny + dy
+                        ## nx, ny is in the board, not air cleaner
+                        if (0 <= temp_x <= R-1 and 0 <= temp_y <= C-1) and board[temp_x][temp_y] != -1:
+                            board[nx][ny] -= spread_air
+                            board[temp_x][temp_y] += spread_air
+
+        # 3) Operate air cleaner
+        temp_board = copy.deepcopy(board)
+        ## shift down & up
+        ### rotate upside
+        for i in range(1, up_cleaner + 1):
+            ## shift down
+            ### if the air cleaner is not in below
+            if i != 1: board[up_cleaner - i + 1][0] = temp_board[up_cleaner - i][0]
+            ## shift up
+            board[i - 1][C - 1] = temp_board[i][C - 1]
+
+        ### rotate downside
+        for i in range(1, R - down_cleaner):
+            ## shift up
+            ### if the air cleaner is not in below
+            if i != 1: board[down_cleaner + i - 1][0] = temp_board[down_cleaner + i][0]
+            ## shift down
+            board[R - i][C - 1] = temp_board[R - i - 1][C - 1]
+
+        ## shift right & left
+        for i in range(1, C):
+            ## shift right
+            if i == 1: board[up_cleaner][i], board[down_cleaner][i] = 0, 0
+            else: board[up_cleaner][i], board[down_cleaner][i] = temp_board[up_cleaner][i - 1], temp_board[down_cleaner][i - 1]
+            ## shift left
+            board[R - 1][i - 1], board[0][i - 1] = temp_board[R - 1][i], temp_board[0][i]
+
+    answer = sum(map(sum, board)) + 2
     return answer
 
 # Input Test
-solution()
+print(solution())
